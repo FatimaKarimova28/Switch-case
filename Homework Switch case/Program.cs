aace938a62f7252ca70be6e808c26bf2f6376622
FatimaKarimova28 <fatimaika@code.edu.az>
2023-01-19 18:30:32 +0400

    ujasssss

---
+++ Homework Switch case/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+ Console.WriteLine("Birinci ededi daxil edin:");
+double number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ikinci ededi daxil edin:");
+double number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Hansi emeliyyati aparmaq istədiyinizi daxil edin:");
+var operation = (Console.ReadLine());
+{
+    switch (operation)
+    {
+        case "+":
+            Console.WriteLine(number1 + number2); break;
+        case "-": Console.WriteLine(number1 - number2); break;
+        case "*": Console.WriteLine(number1 * number2); break;
+        case "/": Console.WriteLine(number1 / number2); break;
+        default:
+            Console.WriteLine("Secim yanlishdir");
+
+            break;
+    }
+}
+
+   
+
+
+
